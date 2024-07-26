9fdb781eddc68ba050addc8eecd812f413990824
nicolasgabriiel_ <nicolasgabriel.developer@gmail.com>
2024-07-26 14:36:40 -0300

    BoardException and defensive programming

---
+++ src/boardgame/Exceptions/BoardException.java
@@ -0,0 +1,9 @@
+package boardgame.Exceptions;
+
+    public class BoardException extends RuntimeException {
+
+        public BoardException(String msg) {
+            super(msg);
+        }
+    }
+
